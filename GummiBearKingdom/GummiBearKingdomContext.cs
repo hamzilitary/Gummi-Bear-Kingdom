7ad3a6bd37fab53294dab1359f4f1fcaa8d6ea10
Hamza Naeem <hamzilitary22@gmail.com>
2018-04-20 09:44:36 -0700

    add HomeController, appsettings.json, and View

---
+++ GummiBearKingdom/GummiBearKingdomContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiBearKingdomContext : DbContext
+    {
+        //public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=gummibearkingdom;uid=root;pwd=root;");
+    }
+}
