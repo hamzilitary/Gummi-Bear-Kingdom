45be69f46d394469f4a5b8de046181abee8099ee
Nico Daunt and Hamza Naeem <nico.daunt@gmail.com and hamzilitary22@gmail.com>
2018-04-27 12:28:39 -0700

    get first test to fail

---
+++ GummiBearKingdom.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        public string Description { get; private set; }
+
+        [TestMethod]
+        public void GetDescription_ReturnsItem_String()
+        {
+            //Arrange
+            var item = new ItemTests();
+            item.Description = "Wash the Gummi";
+
+            //Act 
+            var result = item.Description;
+            
+
+            //Assert 
+            Assert.AreEqual("Wash the Gummi", result);
+
+
+        }
+    }
+}
