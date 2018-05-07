f2a8c57d1a7d4ccf575e0d5eca780b060b125fe6
Cameron Anderson and Ian Goodrich <chamburg321@gmail.com and iangoodrich@hotmail.com>
2018-05-07 15:35:33 -0700

    add IReview and EFReview

---
+++ GummiBearKingdom/Models/Repositories/EFReviewRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class EFReviewRepository : IReviewRepository
+    {
+        GummiBearKingdomDbContext db;
+        public EFReviewRepository()
+        {
+            db = new GummiBearKingdomDbContext();
+        }
+        public EFReviewRepository(GummiBearKingdomDbContext thisDb)
+        {
+            db = thisDb;
+        }
+
+        public IQueryable<Review> Reviews { get { return db.Reviews; } }
+
+        public Review Save(Review review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return review;
+        }
+
+        public void Remove(Review review)
+        {
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+        }
+
+        public void RemoveAll()
+        {
+            db.Database.ExecuteSqlCommand("DELETE FROM products; DELETE FROM reviews");
+        }
+    }
+}
