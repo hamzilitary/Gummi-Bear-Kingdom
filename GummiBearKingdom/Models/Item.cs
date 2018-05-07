dac50e36e30ca108e9860b7e123bfc452a198847
Cameron Anderson and Ian Goodrich <chamburg321@gmail.com and iangoodrich@hotmail.com>
2018-05-07 15:14:18 -0700

    add average rating

--- GummiBearKingdom/Models/Item.cs
+++ GummiBearKingdom/Models/Item.cs
@@ -18,7 +18,50 @@ namespace GummiBearKingdom.Models
         public virtual ICollection<Review> Reviews { get; set; }
 
 
-    }
+    
 
     //Method for Rating will go here
+    public double AvgRating()
+    {
+
+        List<int> ratings = new List<int>();
+        if (ratings.Count > 0)
+        {
+            foreach (var review in Reviews)
+            {
+                ratings.Add(review.Rating);
+            }
+            return Math.Round(ratings.Average());
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+
+    public override bool Equals(System.Object otherProduct)
+    {
+        if (!(otherProduct is Item))
+        {
+            return false;
+        }
+        else
+        {
+            Item newProduct = (Item)otherProduct;
+            return this.ItemId.Equals(newProduct.ItemId);
+        }
+    }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -217289168;
+            hashCode = hashCode * -1521134295 + ItemId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Cost.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Description);
+            hashCode = hashCode * -1521134295 + EqualityComparer<ICollection<Review>>.Default.GetHashCode(Reviews);
+            return hashCode;
+        }
+    }
 }
